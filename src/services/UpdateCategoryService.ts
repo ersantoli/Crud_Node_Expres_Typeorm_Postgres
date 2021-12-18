23b61c3f45442bd27fe3329a53652554d4f3ded3
ersantoli <ersantoli@gmail.com>
2021-12-18 10:11:52 -0300

    primeiro commit

---
+++ src/services/UpdateCategoryService.ts
@@ -0,0 +1,29 @@
+import { getRepository } from "typeorm";
+import { Category } from "../entities/Category";
+
+type CategoryUpdateRequest = {
+    id: string;
+    name: string;
+    description: string;
+
+};
+
+export class UpdateCategoryService {
+    async execute({id, name, description}: Category){
+
+     const  repo = getRepository(Category);
+
+     const category = await repo.findOne(id);
+
+     if(!category){
+         return new Error("Category does not Exists!");
+     }
+
+     category.name = name ? name : category.name;
+     category.description = description ? description : category.description;
+
+     await repo.save(category);
+     return category;
+    }
+     
+}
